84506819c10f60f15c56ac3b282b84ac49881318
Leonardo Kuntz <99610937+scuiki@users.noreply.github.com>
2023-10-09 02:42:29 -0300

    "Green Shop"
    New Screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from './pages/Login/Login';
+import Loja from './pages/Loja/Loja';
 
 function App() {
   return (
-    <div className="App loginScreen">
-      <Login />
+    <div className="App">
+      
+      <Loja />
     </div>
   );
 }
